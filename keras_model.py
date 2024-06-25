f734ff167f80f1d3c6b8bc07d984f571aa174dc6
Dante <128978419+Dante1902@users.noreply.github.com>
2024-06-25 20:21:40 +0800

    Update keras_model.py

--- keras_model.py
+++ keras_model.py
@@ -1,41 +1,131 @@
 from sklearn.preprocessing import LabelBinarizer
 from tensorflow import keras
 from keras.utils import plot_model
+
 import tensorflow as tf
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import pickle
 
-model = keras.models.Sequential([
-    keras.layers.Conv2D(32, (5, 5), padding='same', activation='relu', input_shape=(28, 28, 1)),
-    keras.layers.MaxPooling2D(pool_size=(2, 2)),
-    keras.layers.Conv2D(64, (5, 5), padding='same', activation='relu'),
-    keras.layers.MaxPooling2D(pool_size=(2, 2)),
-    keras.layers.Conv2D(128, (5, 5), padding='same', activation='relu'),
-    keras.layers.MaxPooling2D(pool_size=(2, 2)),
-    keras.layers.Flatten(),
-    keras.layers.Dense(128, activation='relu'),
-    keras.layers.Dense(24, activation='softmax')
-])
+train_df = pd.read_csv('data/alphabet/sign_mnist_train.csv')
+
+train_df = train_df.sample(frac=1, random_state=42) 
+
+X, y = train_df.drop('label', axis=1), train_df['label'] 
+
+X.shape, y.shape
+
+np.unique(X.dtypes), y.dtype
+
+label_binarizer = LabelBinarizer()
+y = label_binarizer.fit_transform(y)
+
+X = X/255.0 
+np.unique(X.dtypes)
+
+
+X = tf.reshape(X, [-1, 28, 28, 1])
+
+X.shape, y.shape
+
+X_train, X_valid = X[:25000], X[25000:]
+y_train, y_valid = y[:25000], y[25000:]
+
+X_train[0].dtype
+X_train[0].shape
+plt.imshow(X[0], cmap='gray'), y[0]
+
+
+model = keras.models.Sequential()
+model.add(keras.layers.Conv2D(32, (5, 5), padding='same', activation='relu', input_shape=(28, 28, 1)))
+model.add(keras.layers.MaxPooling2D(pool_size=(2, 2)))
+model.add(keras.layers.Conv2D(64, (5, 5), padding='same', activation='relu'))
+model.add(keras.layers.MaxPooling2D(pool_size=(2, 2)))
+model.add(keras.layers.Conv2D(128, (5, 5), padding='same', activation='relu'))
+model.add(keras.layers.MaxPooling2D(pool_size=(2, 2)))
+model.add(keras.layers.Flatten())
+model.add(keras.layers.Dense(128, activation='relu'))
+model.add(keras.layers.Dense(24, activation='softmax'))
+
+model.summary()
+
 model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-
-save_best_cb = keras.callbacks.ModelCheckpoint('models/initial-end-to-end', save_best_only=True)
+save_best_cb = keras.callbacks.ModelCheckpoint('models/initial-end-to-end', save_best_only=True) 
 early_stopping_cb = keras.callbacks.EarlyStopping(patience=5)
-history = model.fit(X_train, y_train, epochs=20, validation_data=(X_valid, y_valid), callbacks=[save_best_cb, early_stopping_cb])
 
-test_df = pd.read_csv('D:/pycharm/data/data.csv')
+
+history = model.fit(X_train, y_train, epochs=20, validation_data=(X_valid, y_valid), callbacks=[save_best_cb, early_stopping_cb])
+history.history # Contains the training related information for each epoch
+
+
+h = np.load('models/intial-end-to-end-history', allow_pickle=True)
+h
+
+best_model = keras.models.load_model('models/initial-end-to-end') # Model with best set of parameters not necessarily the model at the last epoch of training
+
+fig, ax = plt.subplots(figsize=(10, 5))
+n_epochs = len(h['loss'])
+ax.plot(range(1, n_epochs+1), h['loss'], color='b', label='train_loss')
+ax.plot(range(1, n_epochs+1), h['val_loss'], color='c', label='val_loss')
+ax.plot(range(1, n_epochs+1), h['accuracy'], color='b', label='train_accuracy', linestyle='--')
+ax.plot(range(1, n_epochs+1), h['val_accuracy'], color='c', label='val_accuracy', linestyle='--')
+ax.set_xticks(range(1, n_epochs+1))
+ax.legend()
+
+
+fig, ax = plt.subplots(figsize=(10, 5))
+n_epochs = len(h['loss'])
+
+x_loss = np.arange(n_epochs+1)-0.5
+ax.plot(x_loss[x_loss >= 0], h['loss'], color='b', label='train_loss')
+ax.plot(range(1, n_epochs+1), h['val_loss'], color='r', label='val_loss')
+ax.plot(range(1, n_epochs+1), h['accuracy'], color='b', label='train_accuracy', linestyle='--')
+ax.plot(range(1, n_epochs+1), h['val_accuracy'], color='r', label='val_accuracy', linestyle='--')
+ax.set_xlim(0, n_epochs)
+ax.set_xticks(range(1, n_epochs+1))
+ax.legend()
+
+
+def get_train_val_plots(h, yticks=None, figsize=(10, 5)):
+    
+    fig, ax = plt.subplots(figsize=figsize)
+    n_epochs = len(h['loss'])
+    x_loss = np.arange(n_epochs+1)-0.5
+    
+    ax.plot(x_loss[x_loss >= 0], h['loss'], color='b', label='train_loss')
+    ax.plot(range(1, n_epochs+1), h['val_loss'], color='r', label='val_loss')
+    ax.plot(range(1, n_epochs+1), h['accuracy'], color='b', label='train_accuracy', linestyle='--')
+    ax.plot(range(1, n_epochs+1), h['val_accuracy'], color='r', label='val_accuracy', linestyle='--')
+    ax.set_xlim(0, n_epochs)
+    ax.set_xticks(range(1, n_epochs+1))
+    if yticks is not None:
+        ax.set_yticks(yticks)
+    ax.legend()
+
+get_train_val_plots(h)
+
+test_df = pd.read_csv('data/alphabet/sign_mnist_test.csv') # Load the test data
 X_test, y_test = test_df.drop('label', axis=1), test_df['label']
 X_test = tf.reshape(X_test, [-1, 28, 28, 1])
-y_test = label_binarizer.transform(y_test)
-best_model.evaluate(X_test, y_test)
 
+Converting Images to a Sentence
+test_df = pd.read_csv('data/alphabet/sign_mnist_test.csv') # Load the test data
+X_test, y_test = test_df.drop('label', axis=1), test_df['label']
+X_test = tf.reshape(X_test, [-1, 28, 28, 1])
 d = {chr(ord('a') + i):i for i in range(26)}
 d_rev = {i:chr(ord('a') + i) for i in range(26)}
 d[' '] = d_rev[' '] = ' '
 
+sentence = 'sign language'
+
+for i in sentence:
+    print(d[i], end=' ')
+best_model.predict(tf.reshape(X_test[0], [-1, 28, 28, 1]))
+
 images_taken = []
 result = ''
+
 for i in sentence:
     if i != ' ':
         char_index = np.random.choice(y_test[y_test==ord(i)-ord('a')].index)
@@ -46,15 +136,66 @@ for i in sentence:
         result += ' '
 print(result)
 
-X_train, X_valid = X[:19500], X[19500:]
-y_train, y_valid = y[:19500], y[
+images_taken_dup = list(reversed(images_taken))
+for word in sentence.split():
+    fig, ax = plt.subplots(1, len(word), figsize=(20, 20))
+    for i in range(len(word)):
+        ax[i].imshow(X_test[images_taken_dup.pop()], cmap='gray')
+        ax[i].set_title(word[i])
+
+
+def test_on_sentence(model, sentence, X_test, y_test, label_binarizer, figsize=(20, 20)):
+
+    sentence = sentence.lower()
+    
+    d = {chr(ord('a') + i):i for i in range(26)}
+    d_rev = {i:chr(ord('a') + i) for i in range(26)}
+    d[' '] = d_rev[' '] = ' '
+    
+    
+    images_taken = []
+    result = ''
+    
+    X_test_reshape = tf.reshape(X_test, [-1, 28, 28, 1])
+    
+
+    for i in sentence:
+        if i != ' ':
+            char_index = np.random.choice(y_test[y_test==ord(i)-ord('a')].index)
+            images_taken.append(char_index)
+            y_pred = model.predict(tf.reshape(X_test_reshape[char_index], [1, 28, 28, 1]))
+            result += d_rev[label_binarizer.inverse_transform(y_pred)[0]]
+        else:
+            result += ' '
+            
+    print(f'The actual sentence is "{sentence}"')
+    print(f'The predicted sentence is "{result}"')
+        
+    images_taken.reverse()
+    for word in sentence.split():
+        fig, ax = plt.subplots(1, len(word), figsize=figsize)
+        for i in range(len(word)):
+            ax[i].imshow(X_test_reshape[images_taken.pop()], cmap='gray')
+            ax[i].set_title(word[i])
+
+test_on_sentence(best_model, 'sign language', test_df.drop('label', axis=1), test_df['label'], label_binarizer)
+
 X_train, X_valid = X[:19500], X[19500:]
 y_train, y_valid = y[:19500], y[19500:]
+
+# Модели
+# 'models/experiment-1-1'
+# 'models/experiment-1-2'
+# 'models/experiment-1-3'
+
+# История
+# 'models/experiment-1-1-history'
+# 'models/experiment-1-2-history'
+# 'models/experiment-1-3-history'
 
 n_pairs = 3
 models_pairs = [keras.models.Sequential() for i in range(n_pairs)]
 early_stopping_cb = keras.callbacks.EarlyStopping(patience=5)
-19500:]
 
 for n in range(1, n_pairs+1):
     models_pairs[n-1].add(keras.layers.Conv2D(32, (5, 5), padding='same', activation='relu', input_shape=(28, 28, 1)))
@@ -70,11 +211,187 @@ for n in range(1, n_pairs+1):
     history = models_pairs[n-1].fit(X_train, y_train, epochs=15, validation_data=(X_valid, y_valid), callbacks=[save_best_cb, early_stopping_cb])
     with open(f'models/experiment-1-{n}-history', 'wb') as history_file:
         pickle.dump(history.history, history_file)
-Вывод архитектуры моделей:
+
 models_pairs[0].summary()
 models_pairs[1].summary()
 models_pairs[2].summary()
 
+for index in range(n_pairs):
+    model = keras.models.load_model(f'models/experiment-1-{index+1}')
+    model.evaluate(X_valid, y_valid)
+
+h_1_1 = np.load('models/experiment-1-1-history', allow_pickle=True)
+h_1_2 = np.load('models/experiment-1-2-history', allow_pickle=True)
+h_1_3 = np.load('models/experiment-1-3-history', allow_pickle=True)
+
+get_train_val_plots(h_1_1, yticks=np.arange(0, 1.2, 0.1))
+get_train_val_plots(h_1_2, yticks=np.arange(0, 1.2, 0.1))
+get_train_val_plots(h_1_3, yticks=np.arange(0, 1.2, 0.1))
+
+Модель 3 имеет наименьшие потери при проверке данных
+
+Фильтры
+Наши модели теперь содержат 3 пары слоев свертки и объединения в пул
+
+Количество карт фильтров может быть
+8 - 16 - 32
+16 - 32 - 64
+24 - 48 - 96
+
+
+# Модели
+# 'models/experiment-fmaps-1'
+# 'models/experiment-fmaps-2'
+# 'models/experiment-fmaps-3'
+
+# История
+# 'models/experiment-fmaps-1-history'
+# 'models/experiment-fmaps-2-history'
+# 'models/experiment-fmaps-3-history'
+
+n_tests = 3
+
+models = []
+early_stopping_cb = keras.callbacks.EarlyStopping(patience=5)
+
+
+for i in range(n_tests):
+    model = keras.models.Sequential()
+    models.append(model)
+    model.add(keras.layers.Input(shape=(28, 28, 1)))
+    for pairs in range(3):
+        model.add(keras.layers.Conv2D((8*(i+1))*(2**pairs), (5, 5), padding='same', activation='relu'))
+        model.add(keras.layers.MaxPooling2D(pool_size=(2, 2)))
+    model.add(keras.layers.Flatten())
+    model.add(keras.layers.Dense(128, activation='relu'))
+    model.add(keras.layers.Dense(24, activation='softmax'))
+    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+    save_best_cb = keras.callbacks.ModelCheckpoint(f'models/experiment-fmaps-{i+1}', save_best_only=True)
+    history = model.fit(X_train, y_train, epochs=10, validation_data=(X_valid, y_valid), callbacks=[save_best_cb, early_stopping_cb])
+    with open(f'models/experiment-fmaps-{i+1}-history', 'wb') as history_file:
+        pickle.dump(history.history, history_file)
+
+models[0].summary()
+models[1].summary()
+models[2].summary()
+
+for index in range(n_tests):
+    model = keras.models.load_model(f'models/experiment-fmaps-{index+1}')
+    model.evaluate(X_valid, y_valid)
+
+h_2_1 = np.load('models/experiment-fmaps-1-history', allow_pickle=True)
+h_2_2 = np.load('models/experiment-fmaps-2-history', allow_pickle=True)
+h_2_3 = np.load('models/experiment-fmaps-3-history', allow_pickle=True)
+
 get_train_val_plots(h_2_1, yticks=np.arange(0, 1.2, 0.1))
 get_train_val_plots(h_2_2, yticks=np.arange(0, 1.2, 0.1))
 get_train_val_plots(h_2_3, yticks=np.arange(0, 1.2, 0.1))
+
+Размер фильтра
+
+# Модели
+# "модели/экспериментальные фильтры-1"
+
+# Объекты истории
+# "модели/экспериментальные фильтры-1-история"
+
+save_best_cb = keras.callbacks.ModelCheckpoint(f'models/experiment-fiters-1', save_best_only=True)
+early_stopping_cb = keras.callbacks.EarlyStopping(patience=5)
+
+model = keras.models.Sequential()
+model.add(keras.layers.Conv2D(24, (3, 3), padding='same', activation='relu'))
+model.add(keras.layers.MaxPooling2D(pool_size=(2, 2)))
+model.add(keras.layers.Conv2D(48, (3, 3), padding='same', activation='relu'))
+model.add(keras.layers.MaxPooling2D(pool_size=(2, 2)))
+model.add(keras.layers.Conv2D(96, (3, 3), padding='same', activation='relu'))
+model.add(keras.layers.MaxPooling2D(pool_size=(2, 2)))
+model.add(keras.layers.Flatten())
+model.add(keras.layers.Dense(128, activation='relu'))
+model.add(keras.layers.Dense(24, activation='softmax'))
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+history = model.fit(X_train, y_train, epochs=10, validation_data=(X_valid, y_valid), callbacks=[save_best_cb, early_stopping_cb])
+with open(f'models/experiment-filters-1-history', 'wb') as history_file:
+    pickle.dump(history.history, history_file)
+
+model = keras.models.load_model('models/experiment-fiters-1/')
+model.evaluate(X_valid, y_valid)
+
+h_2_3 = np.load('models/experiment-fmaps-3-history', allow_pickle=True)
+h = np.load('models/experiment-filters-1-history', allow_pickle=True)
+get_train_val_plots(h, yticks=np.arange(0, 1.2, 0.1))
+get_train_val_plots(h_2_3, yticks=np.arange(0, 1.2, 0.1))
+
+# Модели
+# 'models/experiment-dropout-0'
+# 'models/experiment-dropout-1'
+# 'models/experiment-dropout-2'
+
+# История
+# 'models/experiment-dropout-0-history'
+# 'models/experiment-dropout-1-history'
+# 'models/experiment-dropout-2-history'
+
+early_stopping_cb = keras.callbacks.EarlyStopping(patience=5)
+
+dropout_rates = [0.3, 0.4, 0.5]
+
+for index, i in enumerate(dropout_rates):
+    model = keras.models.Sequential()
+    model.add(keras.layers.Conv2D(24, (5, 5), padding='same', activation='relu'))
+    model.add(keras.layers.MaxPooling2D(pool_size=(2, 2)))
+    model.add(keras.layers.Dropout(i))
+    model.add(keras.layers.Conv2D(48, (5, 5), padding='same', activation='relu'))
+    model.add(keras.layers.MaxPooling2D(pool_size=(2, 2)))
+    model.add(keras.layers.Dropout(i))
+    model.add(keras.layers.Conv2D(96, (5, 5), padding='same', activation='relu'))
+    model.add(keras.layers.MaxPooling2D(pool_size=(2, 2)))
+    model.add(keras.layers.Dropout(i))
+    model.add(keras.layers.Flatten())
+    model.add(keras.layers.Dense(128, activation='relu'))
+    model.add(keras.layers.Dropout(i))
+    model.add(keras.layers.Dense(24, activation='softmax'))
+    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+    save_best_cb = keras.callbacks.ModelCheckpoint(f'models/experiment-dropout-{index}', save_best_only=True)
+    history = model.fit(X_train, y_train, epochs=10, validation_data=(X_valid, y_valid), callbacks=[save_best_cb, early_stopping_cb])
+    with open(f'models/experiment-dropout-{index}-history', 'wb') as history_file:
+        pickle.dump(history.history, history_file)
+
+for index in range(3):
+    model = keras.models.load_model(f'models/experiment-dropout-{index}')
+    model.evaluate(X_valid, y_valid)
+
+h_0 = np.load('models/experiment-dropout-0-history', allow_pickle=True)
+h_1 = np.load('models/experiment-dropout-1-history', allow_pickle=True)
+h_2 = np.load('models/experiment-dropout-2-history', allow_pickle=True)
+
+get_train_val_plots(h_0)
+get_train_val_plots(h_1)
+get_train_val_plots(h_2)
+
+data_augmentation = keras.models.Sequential()
+data_augmentation.add(keras.layers.RandomRotation(0.1, fill_mode='nearest', input_shape=(28, 28, 1)))
+data_augmentation.add(keras.layers.RandomZoom((0.15, 0.2), fill_mode='nearest'))
+data_augmentation.add(keras.layers.RandomTranslation(0.1, 0.1, fill_mode='nearest'))
+
+model = keras.models.Sequential()
+# Добавить слои из приведенной выше модели
+
+best_model = keras.models.load_model('models/experiment-dropout-0/')
+plot_model(best_model, to_file='model.png', show_shapes=True, show_layer_names=True)
+
+
+Performance on the Test Set
+
+test_df = pd.read_csv('data/alphabet/sign_mnist_test.csv')
+X_test, y_test = test_df.drop('label', axis=1), test_df['label']
+best_model = keras.models.load_model('models/experiment-dropout-0/')
+evaluate_model(best_model, X_test, y_test, label_binarizer)
+
+test_df = pd.read_csv('data/alphabet/sign_mnist_test.csv')
+X_test, y_test = test_df.drop('label', axis=1), test_df['label']
+
+# Применяя нормализацию, которая применяется для X_train
+X_test /= 255.0
+
+best_model = keras.models.load_model('models/experiment-dropout-0/')
+evaluate_model(best_model, X_test, y_test, label_binarizer)
